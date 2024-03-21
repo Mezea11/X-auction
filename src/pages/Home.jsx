c5acf91033d7a9c11575eb67d092b54a57b78416
Mezea11 <christiancastellanosmeza@gmail.com>
2024-03-21 10:03:47 +0100

    removed buttons from home
    Co-authored-by: Heli <helihard@users.noreply.github.com>
    Co-authored-by: linnclaesson <linnclaesson@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -101,36 +101,6 @@ function Home() {
             </div>
           </div>
         </div>
-
-        <>
-          <button type="button" className="btn btn-primary">
-            Primary
-          </button>
-          <button type="button" className="btn btn-secondary">
-            Secondary
-          </button>
-          <button type="button" className="btn btn-success">
-            Success
-          </button>
-          <button type="button" className="btn btn-danger">
-            Danger
-          </button>
-          <button type="button" className="btn btn-warning">
-            Warning
-          </button>
-          <button type="button" className="btn btn-info">
-            Info
-          </button>
-          <button type="button" className="btn btn-light">
-            Light
-          </button>
-          <button type="button" className="btn btn-dark">
-            Dark
-          </button>
-          <button type="button" className="btn btn-link">
-            Link
-          </button>
-        </>
       </div>
       <SignupButton />
     </>
