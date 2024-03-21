b3b63c09b2bdb123add09a15e8562e301dd7c2de
Mezea11 <christiancastellanosmeza@gmail.com>
2024-03-21 11:31:50 +0100

    initial layout for navbar
    Co-authored-by: linnclaesson <linnclaesson@users.noreply.github.com>
    Co-authored-by: Heli <helihard@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,56 @@
+import SignupButton from "../components/SignupButton";
+import SignupModal from "../components/SignupButton";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <Link to={"/"} className="navbar-brand">
+              X-Auction
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <div id="nav-link-container">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item" id="">
+                    <a className="nav-link" href="#">
+                      FAQ
+                    </a>
+                  </li>
+                  <li className="nav-item" id="">
+                    <a className="nav-link" href="#">
+                      Log In
+                    </a>
+                  </li>
+                  <li className="nav-item" id="">
+                    <a className="nav-link" href="#">
+                      Create account
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
--- src/components/SignupModal.jsx
+++ src/components/SignupModal.jsx
@@ -1,27 +1,35 @@
-import SignupForm from './SignupForm.jsx';
-import { useState } from 'react';
-
+import SignupForm from "./SignupForm.jsx";
+import { useState } from "react";
 
 export default function SignupModal({ closeModal }) {
-
-    return (
-        <>
-            <div className="modal" id="signupModal" tabIndex="-1" role="dialog" 
-            style={{ display: 'flex', position: 'initial'}}>
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Sign Up</h5>
-                            <button type="button" className="close btn btn-primary" onClick={closeModal} aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <SignupForm />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div
+        className="modal"
+        id="signupModal"
+        tabIndex="-1"
+        role="dialog"
+        style={{ display: "flex", position: "initial" }}
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Sign Up</h5>
+              <button
+                type="button"
+                className="close btn btn-primary"
+                onClick={closeModal}
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-        </>
-    );
+            <div className="modal-body">
+              <SignupForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
