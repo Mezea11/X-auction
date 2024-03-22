d8799aff69c0a5334336193e1185d4a31f9863aa
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-22 14:04:31 +0100

    live search with mock data

---
+++ src/assets/mockproducts.jsx
@@ -0,0 +1,67 @@
+const products = [
+  {
+    name: "Bättre begagnat bungyjumprep",
+    id: 12,
+    description: "Använt en gång, d va inget för mig",
+    askingprice: 2500,
+    image: "",
+    categories: ["fågel"],
+    keywords: ["berg", "bro", "höghöjd"],
+  },
+  {
+    name: "Fallskärm för den händige äventyraren",
+    id: 15,
+    description:
+      "Den har några hål (jag har lagat dem med spinnakertejp för nu) men mycket kvar att ge. Ett perfekt handarbetsprojekt för dig som saknar självbevarelsedrift!",
+    askingprice: 5000,
+    image: "",
+    categories: ["fågel"],
+    keywords: ["flygplan", "livsförakt"],
+  },
+  {
+    name: "Snorkel",
+    id: 25,
+    description: "Något förkortat efter tête-à-tête med <3-full hammarhaj.",
+    askingprice: 150,
+    image: "",
+    categories: ["fisk"],
+    keywords: ["dykning", "breatharian", "wildlife"],
+  },
+  {
+    name: "Rollerskates",
+    id: 32,
+    description:
+      "Äkta vintage, superfint skick! Med dessa på blir du hetast på banan och kan blocka vilken jammer som helst som en QUEEN",
+    askingprice: 1000,
+    image: "",
+    categories: ["mittemellan"],
+    keywords: [
+      "80-tal",
+      "girls just wanna be fast amirite",
+      "I like big bruises and I cannot lie",
+      "Babe Ruthless",
+    ],
+  },
+  {
+    name: "En riktig vinnarpuck",
+    id: 58,
+    description:
+      "Pucken som Finland spelade till seger vid det första VM i undervattensishockey i Österrike 2007",
+    askingprice: 10000,
+    image: "",
+    categories: ["fisk"],
+    keywords: ["Finland", "kallt"],
+  },
+  {
+    name: "Lektion i parkour (intermediate)!",
+    id: 72,
+    description:
+      "Du budar på en parkour-lektion med mig som hållit på med parkour sedan tidiga 20-årsåldern (nu 42). Jag haltar ganska svårt efter en liten olycka men kan coacha dig mer än väl från marken. Obs: du behöver ha levlat förbi nybörjarnivå och känna dig säker på foten, du behöver också ha giltig olycksfallsförsäkring (du vill inte chansa med det sistnämnda)",
+    askingprice: 5000,
+    image: "",
+    categories: ["mittemellan"],
+    keywords: ["fågel?", "urban", "starka ben"],
+  },
+];
+
+export default products;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,65 @@
 //import SignupButton from "../components/SignupButton";
 //import LoginButton from "../components/LoginButton";
+//import Searchbar from "../components/Searchbar.jsx";
+import mockproducts from "../assets/mockproducts.jsx";
+import { useState } from "react";
 
 function Home() {
+  /* HOOK: fångar sökord som användaren matar in - från början tom sträng */
+  const [searchItem, setSearchItem] = useState("");
+  /* HOOK: filtrera sökta produkter - från början (när inget söks) "matchar" alla objekt i mockproducts */
+  const [filteredItems, setFilteredItems] = useState(mockproducts);
+
+  const handleSearchInputChange = (event) => {
+    /* ta in value av search och sätt till lowercase i variable searchTerm - setSearchItem förändrar värdet av searchItem */
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchItem(searchTerm);
+
+    const filteredItems = mockproducts.filter((product) => {
+      /* ta ut matchande sökning baserat på produktens namn */
+      const matchingName =
+        product.name && product.name.toLowerCase().includes(searchTerm);
+      /* ta ut matchande sökning baserat på produktens beskrivning */
+      const matchingDescription =
+        product.description &&
+        product.description.toLocaleLowerCase().includes(searchTerm);
+      const matchingKeywords =
+        product.keywords &&
+        product.keywords.some((keyword) =>
+          keyword.toLowerCase().includes(searchTerm)
+        );
+
+      return matchingName || matchingDescription || matchingKeywords;
+    });
+
+    /* ändrar värdet av filteredItems till en array av objekt där name och/eller description matchar sökning */
+    setFilteredItems(filteredItems);
+  };
   return (
     <>
       <div className="classContainer">
-        <div className="card" style={{ width: "18rem" }}>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            value={searchItem}
+            placeholder="Search"
+            aria-label="Search"
+            onChange={handleSearchInputChange}
+          />
+        </form>
+        <div className="mockdataoutput">
+          {filteredItems.map((product) => (
+            <div key={product.id}>
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <h6>
+                {product.keywords.map((keyword) => "#" + keyword).join(" ")}
+              </h6>
+            </div>
+          ))}
+        </div>
+        {/* <div className="card" style={{ width: "18rem" }}>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
@@ -16,7 +70,7 @@ function Home() {
               Go somewhere
             </a>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
