ccea95b633b98971cbb63894a70d5b1f79592c9f
Heli Hård Källoff <vierge.hard@gmail.com>
2024-04-05 15:31:46 +0200

    added some comments

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,38 +1,41 @@
 import { createContext } from "react";
 import { useState } from "react";
 export const Context = createContext();
-export function ContextProvider({children}){
-    const [LoggedIn, setLoggedIn] = useState(false);
+export function ContextProvider({ children }) {
+  const [LoggedIn, setLoggedIn] = useState(false);
 
-    const logIn=()=>{setLoggedIn(true);}
+  //obs! exempelfunktioner vi skrev bara för att testa funktionalitet
+  const logIn = () => {
+    setLoggedIn(true);
+  };
 
-    const logOut=()=>{setLoggedIn(false);}
+  const logOut = () => {
+    setLoggedIn(false);
+  };
 
-    function handleSubmit(e) {
-        e.preventDefault(); //stop page reload
-        setLoggedIn(true);
+  //obs! denna blev inte riktigt klar men vi har tagit den från riktiga login
+  function handleSubmit(e) {
+    e.preventDefault(); //stop page reload
+    setLoggedIn(true);
 
-        const formData = {
-            email: email,
-            password: password,
-        };
+    const formData = {
+      email: email,
+      password: password,
+    };
 
-        console.log(LoggedIn, formData);
-      }
+    console.log(LoggedIn, formData);
+  }
 
-    
-    return(
-        
-        <Context.Provider
-        value={{
-            LoggedIn,
-            logIn,
-            logOut,
-            handleSubmit,
-        }}
-        >
-          {children}
-        </Context.Provider>
-    )
-    
-};
+  return (
+    <Context.Provider
+      value={{
+        LoggedIn,
+        logIn,
+        logOut,
+        handleSubmit,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+}
