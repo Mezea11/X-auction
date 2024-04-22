a34b07b88dac249b1833e80c0d22c67af069338b
Mezea11 <christiancastellanosmeza@gmail.com>
2024-04-22 09:31:07 +0200

    small fix, fixed broken import

--- server/model/Product.js
+++ server/model/Product.js
@@ -1,29 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // Creates "products" schema: defines the structure of every "product" document in our database
 const productsSchema = new mongoose.Schema({
-    productname: String,
-    description: String,
-    extended_description: String,
-    category: String,
-    keywords: [String],
-    end_dateTime: Date,
-    starting_price: Number,
-    img_url: String,
-    seller: String,
-    ongoing: Boolean,
-    bids: [
-        {
-            username: String,
-            bid: Number,
-        },
-    ],
+  productname: String,
+  description: String,
+  extended_description: String,
+  category: String,
+  keywords: [String],
+  end_dateTime: Date,
+  starting_price: Number,
+  img_url: String,
+  seller: String,
+  ongoing: Boolean,
+  bids: [
+    {
+      username: String,
+      bid: Number,
+    },
+  ],
 });
 
 /* 
   Creates mongoose model based on productsSchema
   Allows us to create/read/update/delete (CRUD) documents in our "products" collection
 */
-const Product = mongoose.model('products', productsSchema);
+const Product = mongoose.model("products", productsSchema);
 
 export default Product;
