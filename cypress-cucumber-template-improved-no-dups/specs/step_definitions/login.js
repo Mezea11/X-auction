bc1733f835b0925bc24b3a1f98f7772312588796
Mezea11 <christiancastellanosmeza@gmail.com>
2024-04-25 16:13:18 +0200

    updated login test: login now tests wrong user credentials.

--- cypress-cucumber-template-improved-no-dups/specs/step_definitions/login.js
+++ cypress-cucumber-template-improved-no-dups/specs/step_definitions/login.js
@@ -12,13 +12,28 @@ Then("the login modal opens", () => {
   cy.get(".modal-content").should("be.visible");
 });
 
-Then("I type in correct user info", () => {
-  cy.get("#username").type("kalleboll");
-  cy.get("#password").type("12345");
+// Wrong user info
+Then("I type in incorrect user info", () => {
+  cy.get("#username").type("48645");
+  cy.get("#password").type("456898");
 });
 
-Then("I press the Login button", () => {
+When("I press the Login button", () => {
   cy.get(".login-form > .btn").click();
+});
+
+When("Incorrect username or password. Please try again.", () => {
+  cy.get("#alert-message").should("be.visible");
+});
+
+Then("I clear the form", () => {
+  cy.get("#username").clear(); // Clear the username field
+  cy.get("#password").clear(); // Clear the password field
+});
+
+When("I type in correct user info", () => {
+  cy.get("#username").type("kalleboll");
+  cy.get("#password").type("12345").type("{enter}");
 });
 
 Then("I get logged in", () => {
