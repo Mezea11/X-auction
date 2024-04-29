de38620bdb2f8fb7f114c232dade4ef77b676680
PelleHanspers <p.hanspers@gmail.com>
2024-04-29 15:21:32 +0200

    removed json db fetch from PostProductModal

--- client/src/components/PostProductModal.jsx
+++ client/src/components/PostProductModal.jsx
@@ -4,43 +4,7 @@ import { useState, useEffect } from 'react';
 export default function PostProductModal({ closeModal }) {
     const [isSuccess, setIsSuccess] = useState(false);
 
-    const postProduct = async (
-        title,
-        description,
-        extended_Description,
-        category,
-        keywords,
-        endDate,
-        price,
-        img_url
-    ) => {
-        try {
-            const parsedPrice = parseInt(price);
-
-            const response = await fetch('http://localhost:3000/products', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    title,
-                    description,
-                    extended_Description,
-                    category,
-                    keywords,
-                    endDate,
-                    price: parsedPrice,
-                    img_url,
-                }),
-            });
-            if (!response.ok) {
-                throw new Error('Failed to post product');
-            }
-
-            //fetchAllProducts();
-        } catch (error) {
-            console.error('Error posting product:', error);
-        }
+    const postProduct = async () => {
         setIsSuccess(true);
     };
 
